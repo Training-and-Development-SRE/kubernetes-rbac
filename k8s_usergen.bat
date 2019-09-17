932898c97ddc76b4e281c1b81dec5b4ea058543d
airwavetechio-gh <tony@airwavetech.io>
2019-09-16 18:11:48 -0700

    the beginnings of the tutorial

---
+++ k8s_usergen.bat
@@ -0,0 +1,19 @@
+SET _USERNAME=%1
+SET _CA_LOCATION=%USERPROFILE%
+SET _OUTPUTDIR=users\%_USERNAME%
+SET _SERVERURL=%2
+SET _CLUSTERNAME=minikube
+rmdir users\%_USERNAME%
+mkdir %_OUTPUTDIR%
+
+IF [%1] == [] echo "Missing username parameter" && cmd /k
+IF [%2] == [] echo "Missing server URL parameter" && cmd /k
+openssl genrsa -out %_OUTPUTDIR%\%_USERNAME%.key 2048
+openssl req -new -key  %_OUTPUTDIR%\%_USERNAME%.key -out %_OUTPUTDIR%\%_USERNAME%.csr -subj "/CN=%_USERNAME%/O=airwave"
+openssl x509 -req -in %_OUTPUTDIR%\%_USERNAME%.csr -CA %_CA_LOCATION%\.minikube\ca.crt -CAkey %_CA_LOCATION%\.minikube\ca.key -CAcreateserial -out %_OUTPUTDIR%\%_USERNAME%.crt -days 500
+timeout /t 1
+
+kubectl config --kubeconfig=%_OUTPUTDIR%\config set-cluster minikube --certificate-authority=%_CA_LOCATION%\.minikube\ca.crt --server=%_SERVERURL% --embed-certs=true
+kubectl config --kubeconfig=%_OUTPUTDIR%\config set-credentials %_USERNAME% --client-certificate=%_OUTPUTDIR%\%_USERNAME%.crt  --client-key=%_OUTPUTDIR%\%_USERNAME%.key --embed-certs=true
+kubectl config --kubeconfig=%_OUTPUTDIR%\config set-context airwave-rbac --cluster=minikube --namespace=airwave-rbac --user=%_USERNAME% 
+kubectl config --kubeconfig=%_OUTPUTDIR%\config use-context airwave-rbac
