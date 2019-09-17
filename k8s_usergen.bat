83fe1ae606cd6ee0df0ec992e0c4f89931d4968c
airwavetechio-gh <tony@airwavetech.io>
2019-09-16 20:01:34 -0700

    User scripts almost ready
    Need to figure out groups next

--- k8s_usergen.bat
+++ k8s_usergen.bat
@@ -1,5 +1,6 @@
+@ECHO OFF
 SET _USERNAME=%1
-SET _CA_LOCATION=%USERPROFILE%
+SET _CA_LOCATION=%USERPROFILE%\minikube
 SET _OUTPUTDIR=users\%_USERNAME%
 SET _SERVERURL=%2
 SET _CLUSTERNAME=minikube
@@ -10,10 +11,18 @@ IF [%1] == [] echo "Missing username parameter" && cmd /k
 IF [%2] == [] echo "Missing server URL parameter" && cmd /k
 openssl genrsa -out %_OUTPUTDIR%\%_USERNAME%.key 2048
 openssl req -new -key  %_OUTPUTDIR%\%_USERNAME%.key -out %_OUTPUTDIR%\%_USERNAME%.csr -subj "/CN=%_USERNAME%/O=airwave"
-openssl x509 -req -in %_OUTPUTDIR%\%_USERNAME%.csr -CA %_CA_LOCATION%\.minikube\ca.crt -CAkey %_CA_LOCATION%\.minikube\ca.key -CAcreateserial -out %_OUTPUTDIR%\%_USERNAME%.crt -days 500
+openssl x509 -req -in %_OUTPUTDIR%\%_USERNAME%.csr -CA %_CA_LOCATION%\ca.crt -CAkey %_CA_LOCATION%\ca.key -CAcreateserial -out %_OUTPUTDIR%\%_USERNAME%.crt -days 500
 timeout /t 1
 
-kubectl config --kubeconfig=%_OUTPUTDIR%\config set-cluster minikube --certificate-authority=%_CA_LOCATION%\.minikube\ca.crt --server=%_SERVERURL% --embed-certs=true
+kubectl config --kubeconfig=%_OUTPUTDIR%\config set-cluster minikube --certificate-authority=%_CA_LOCATION%\ca.crt --server=%_SERVERURL% --embed-certs=true
 kubectl config --kubeconfig=%_OUTPUTDIR%\config set-credentials %_USERNAME% --client-certificate=%_OUTPUTDIR%\%_USERNAME%.crt  --client-key=%_OUTPUTDIR%\%_USERNAME%.key --embed-certs=true
 kubectl config --kubeconfig=%_OUTPUTDIR%\config set-context airwave-rbac --cluster=minikube --namespace=airwave-rbac --user=%_USERNAME% 
 kubectl config --kubeconfig=%_OUTPUTDIR%\config use-context airwave-rbac
+
+
+echo. >> rbac-user-rolebinding.yml
+echo --- >> rbac-user-rolebinding.yml
+echo apiVersion: v1 >> rbac-user-rolebinding.yml
+echo kind: ServiceAccount >> rbac-user-rolebinding.yml
+echo metadata: >> rbac-user-rolebinding.yml
+echo   name: %_USERNAME% >> rbac-user-rolebinding.yml
---
+++ k8s_usergen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+_USERNAME=$1
+_CA_LOCATION=./server_certs
+_OUTPUTDIR=users/$_USERNAME
+_SERVERURL=$2
+_CLUSTERNAME=minikube
+
+mkdir -p $_OUTPUTDIR
+
+if [ -z $1 ]; then 
+    echo "Missing username parameter";
+    exit 1;
+fi
+if [ -z $2 ]; then 
+    echo "Missing server URL parameter";
+    exit 1;
+fi
+openssl genrsa -out $_OUTPUTDIR/$_USERNAME.key 2048
+openssl req -new -key  $_OUTPUTDIR/$_USERNAME.key -out $_OUTPUTDIR/$_USERNAME.csr -subj "/CN=$_USERNAME/O=airwave"
+openssl x509 -req -in $_OUTPUTDIR/$_USERNAME.csr -CA $_CA_LOCATION/ca.crt -CAkey $_CA_LOCATION/ca.key -CAcreateserial -out $_OUTPUTDIR/$_USERNAME.crt -days 500
+sleep 2
+
+kubectl config --kubeconfig=$_OUTPUTDIR/config  set-cluster minikube --certificate-authority=$_CA_LOCATION/ca.crt --server=$_SERVERURL --embed-certs=true
+kubectl config --kubeconfig=$_OUTPUTDIR/config set-credentials $_USERNAME --client-certificate=$_OUTPUTDIR/$_USERNAME.crt  --client-key=$_OUTPUTDIR/$_USERNAME.key --embed-certs=true
+kubectl config --kubeconfig=$_OUTPUTDIR/config set-context airwave-rbac --cluster=minikube --namespace=airwave-rbac --user=$_USERNAME 
+kubectl config --kubeconfig=$_OUTPUTDIR/config use-context airwave-rbac
+
+echo -e "\n---\napiVersion: v1\nkind: ServiceAccount\nmetadata:\n    name: $_USERNAME" >> rbac-user-rolebinding.yml
+
